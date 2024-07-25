ebae52f06986f6e0459e4bdcf7a9b5355262c598
punkrelique <blackbull0007@gmail.com>
2024-07-26 01:19:50 +0300

    init

---
+++ README.md
@@ -0,0 +1,39 @@
+# Squaddy
+This project fetches and displays game statistics for a specified Steam user using the MySquadStats and Steam APIs. The stats include the playtime of a [Squad](https://store.steampowered.com/app/393380/Squad/) and are displayed in a tabular format in the console.
+
+## Requirements
+- [.NET SDK](https://dotnet.microsoft.com/download)
+- An environment variable `SQUADDY_STEAM_API_KEY` set with your Steam API key.
+
+## Setup
+1. **Clone the repository**:
+```sh  
+git clone <repository-url>
+```
+```sh
+cd <repository-directory> 
+```
+2. **Set the Steam API Key**:  
+Make sure to set the `SQUADDY_STEAM_API_KEY` environment variable with your Steam API key:
+```sh 
+export SQUADDY_STEAM_API_KEY=your_steam_api_key
+```  
+3. **Run the script**:  
+Execute the script with the required arguments:
+```sh 
+dotnet fsi script.fsx <username> <caseComparison> <matchType> 
+```  
+- `<username>`: The username to search for.
+    - `<caseComparison>`: The case comparison type. Options are:
+        -  `ignorecase` or `followcase`.
+    - `<matchType>`: The match type. Options are:
+        -  `contains` or `exactly`.
+
+## Example
+To run the script for a user with username "exampleUser", case-insensitive comparison, and exact match:
+```sh
+dotnet fsi squaddy.fsx "exampleUser" ignorecase exactly
+```
+
+## Thanks to
+- [My Squad Stats](https://mysquadstats.com/)
---
+++ squaddy.fsx
@@ -0,0 +1,70 @@
+open System
+open System.Net.Http
+open System.Net.Http.Json
+
+let args = fsi.CommandLineArgs[1..]
+
+if args.Length = 0 then
+    printf "Username was not provided"
+    Environment.Exit(-1)
+    
+let username = args[0]
+
+let caseComparison =
+    match Array.tryItem 1 args with
+    | Some value when value = "ignorecase" -> StringComparison.OrdinalIgnoreCase
+    | Some value when value = "followcase" -> StringComparison.Ordinal
+    | _                                    -> StringComparison.Ordinal
+    
+let findString =
+    match Array.tryItem 2 args with
+    | Some value when value = "contains"   -> fun (str1: string) (str2: string) -> str1.Contains(str2, caseComparison)
+    | Some value when value = "exactly"    -> fun (str1: string) (str2: string) -> str1.Equals(str2, caseComparison)
+    | _                                    -> fun (str1: string) (str2: string) -> str1.Equals(str2, caseComparison)
+
+let steamApiKey = Environment.GetEnvironmentVariable("SQUADDY_STEAM_API_KEY")
+if String.IsNullOrEmpty(steamApiKey) then
+    printfn "SQUADDY_STEAM_API_KEY environment variable is not set"
+    Environment.Exit(-1)
+
+type mySquadStats = { data: seq<{| lastName: string; steamID: string |}>; successStatus: string; successMessage: string }
+let mySquadStatsUrlTemplate username = $"https://mysquadstats.com/api/players?search={username}"
+let getPlayerStats = task {
+    use client = new HttpClient()
+    client.DefaultRequestHeaders.Referrer <- Uri("https://mysquadstats.com/")
+    let! response = client.GetAsync(mySquadStatsUrlTemplate username)
+    let! content = response.Content.ReadFromJsonAsync<mySquadStats>()
+    
+    return content.data
+}
+
+let playerStats = getPlayerStats |> Async.AwaitTask |> Async.RunSynchronously
+
+let steamStatsUrlTemplate steamId = $"https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={steamApiKey}&steamid={steamId}&format=json&&appids_filter[0]=393380"
+type steamInfo = { response: {| game_count: int; games: Option<seq<{| appid: int; playtime_2weeks: int; playtime_forever: int |}>> |} }
+let steamClient = new HttpClient()
+let getSteamUserGameStats steamApiKey steamId = task {
+    let! response = steamClient.GetAsync(steamStatsUrlTemplate steamId)
+    let! content = response.Content.ReadFromJsonAsync<steamInfo>()
+    
+    return content
+}
+
+playerStats
+    |> Seq.where (fun x -> findString (x.lastName.TrimStart()) username)
+    |> Seq.mapi (fun i squadStatsUser -> 
+        let hours = 
+            (getSteamUserGameStats steamApiKey squadStatsUser.steamID
+            |> Async.AwaitTask
+            |> Async.RunSynchronously).response.games
+            |> function
+                | Some games -> games |> Seq.tryLast |> Option.map (fun x -> x.playtime_forever / 60) |> Option.defaultValue 0
+                | None -> 0
+        
+        printfn "%-2d %-20s %-60s %-10d %s"
+            (i + 1)
+            squadStatsUser.lastName
+            ("https://steamcommunity.com/profiles/" + squadStatsUser.steamID)
+            hours
+            (if hours = 0 then "Private" else "Visible")
+    )
